d3411981d271c026b1a975418553e609cf7c173f
Magesh Kuppan <tkmagesh77@gmail.com>
2024-05-16 22:21:54 +0530

    web server serving file resources

--- 08-web-server/server.js
+++ 08-web-server/server.js
@@ -4,12 +4,24 @@ const http = require('node:http'),
 
 const server = http.createServer((req /* http.IncomingMessage */, res /* http.ServerResponse */) => {
     console.log(`${req.method} - ${req.url}`);
-    const resourceRequested = req.url;
+    const resourceRequested = req.url === '/' ? 'index.html' : req.url;
     const resourceFullPath = path.join(__dirname, resourceRequested)
     if (fs.existsSync(resourceFullPath)) {
-    //   res.write("resource found - will send the resource shortly!");
-      // read the file and send to the client
-      res.end();
+      const stream = fs.createReadStream(resourceFullPath);
+      /* 
+      stream.on('data', chunk => {
+        res.write(chunk);
+      });
+      stream.on('end', () => {
+        res.end();
+      }) 
+      */
+      stream.on('error', err => {
+        console.log('error :', err);
+        res.statusCode = 500;
+        res.end('internal server error')
+      });
+      stream.pipe(res);
     } else {
       res.statusCode = 404;
       res.end("resource not found");
